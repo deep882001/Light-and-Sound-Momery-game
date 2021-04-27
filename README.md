7d245a98c60cd09bcdd74ac92d955c3a24238ba1
Deep Patel <56002273+deep882001@users.noreply.github.com>
2021-04-26 20:48:57 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Light-and-Sound-Momery-game
+# simon-game
+
+  '**Windows Square**' clone of the classic Simon Game, as a tribute to Microsoft Windows' color scheme.
+
+  The app is served on 🔥 [Firebase](https://square-simon-game.firebaseapp.com/).
+
+  ![Windows Square](./pictures/gameOver-screenshot.png?raw=true "Windows Square")
+
+# Features
+  * User is presented with a random series of button presses
+  * Each time user inputs a series button presses correctly, be presented with the same series of button presses but with an additional step
+    * Hear a sound corresponding to each button both when **the series of button presses play**, and **when user presses a button**
+  * If press the wrong button, user is notified of that and that series of button presses starts again so user can try again
+  * User can see how many steps are in the current series of button presses
+  * User can end the current game session at any time and start over
+  * **Strict Mode:** game session will reset once the user presses the wrong button (current step go down to 0)
+  * User can win the game by getting a series of 20 steps correct
+  * **Fits both large and small screens (break point at 845px)**
---
+++ src/Game.js
@@ -0,0 +1,272 @@
+import React, { Component } from 'react';
+
+import './Game.css';
+
+const GREEN = '#7cbb00';
+const RED = '#f65314';
+const YELLOW = '#ffbb00';
+const BLUE = '#00a1f1';
+const COLORARR = [GREEN, RED, YELLOW, BLUE];
+const SOUND1 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+const SOUND2 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+const SOUND3 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+const SOUND4 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+const SOUNDS = [SOUND1, SOUND2, SOUND3, SOUND4];
+// load audio
+for (let sound of SOUNDS) {
+	sound.load();
+}
+const MESSAGE = {
+	'gameOver': 'Game Over',
+	'wrong': 'Oops.',
+	'correct': 'Correct!',
+	'won': 'You\' beaten the game!'
+};
+
+const Button = props => {
+	return (
+		<button
+			className={props.className}
+			style={{ backgroundColor: props.color, color: 'white' }}
+			onMouseDown={props.onMouseDown}
+			disabled={!props.clickable}
+			id={props.index}
+		/>
+	);
+};
+
+class Game extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			colors: COLORARR,
+			sequence: [],
+			stepCount: 0,
+			buttonClickable: false,
+			intervalKey: null,
+			gameStarted: false,
+			highScore: 0,
+			isStrict: false,
+			gameMessage: '',
+			timeOutKey: null
+		};
+	}
+
+	// present user with random button presses
+	lightUp = () => {
+		const colors = this.state.colors;
+		let sequence = this.state.sequence;
+		let count = 0;
+
+		const interval = setInterval(() => {
+			let colorIndex = colors.indexOf(sequence[count]);
+			// play sound
+			playSoundAtIndex(colorIndex);
+			// retrieve button to add 'lightup' class
+			let button = document.getElementById(colorIndex);
+			button.classList.add('lightup');
+			// wait 800ms to remove 'lightup' class
+			setTimeout(() => {
+				button.classList.remove('lightup');
+			}, 800);
+			count += 1;
+			if (count >= sequence.length) {
+				// clears lightup interval once all lights in sequence are done
+				clearInterval(interval);
+			}
+		}, 1200);
+		// set intervalKey for clearing setInterval when click wrong button
+		this.setState({
+			intervalKey: interval
+		});
+	};
+
+	// when click 'start' button, display sequence & set button to clickable once finished
+	startGame = addNewColor => {
+		this.setState({
+			gameStarted: true,
+			gameMessage: '',
+		});
+		// if clicked correct AND we're at length 20, don't add new color, instead game won
+		if (addNewColor && this.state.sequence.length >= 20) {
+			this.endGame();
+			return;
+		} else if (addNewColor) {
+			const sequence = this.state.sequence;
+			sequence.push(generateNextColor(COLORARR));
+			// at beginning of each round, push one random color to sequence
+			this.setState({
+				sequence,
+			});
+		}
+
+		// play sequence
+		this.lightUp();
+		// wait until lightup finished then make button clickable
+		const timeOutKey = setTimeout(() => {
+			this.setState({
+				buttonClickable: true
+			});
+		}, 1200 * this.state.sequence.length + 500);
+		this.setState({ timeOutKey })
+		return;
+	};
+
+	// ending game
+	endGame = () => {
+		clearInterval(this.state.intervalKey);
+		clearTimeout(this.state.timeOutKey);
+		const sequenceLength = this.state.sequence.length;
+		this.setState({
+			sequence: [],
+			stepCount: 0,
+			buttonClickable: false,
+			intervalKey: null,
+			gameStarted: false,
+			isStrict: false,
+			gameMessage: sequenceLength >= 20 ? MESSAGE['won'] : MESSAGE['gameOver'],
+		});
+	};
+
+	// toggle Strict mode
+	toggleStrict = () => {
+		this.setState(prevState => {
+			return {
+				isStrict: !prevState.isStrict
+			};
+		});
+		return;
+	};
+
+	restartSequence = () => {
+		clearInterval(this.state.intervalKey);
+		// 1 clear interval using this.state.intervalKey
+		// 2 wait 500ms to restart game // TODO - flash indicator to indicate wrong input
+		setTimeout(() => {
+			this.startGame(false);
+		}, 2000);
+		this.setState(prevState => {
+			return {
+				buttonClickable: false,
+				intervalKey: null,
+				stepCount: 0,
+			};
+		});
+
+	};
+
+	
+
+	handleColorMouseDown = (color, index) => {
+		// color is color of the button
+		// index to play sound
+		const stepCount = this.state.stepCount;
+		const sequence = this.state.sequence;
+		// .load() fixes problem of audio playing only once
+		playSoundAtIndex(index);
+		if (color !== sequence[stepCount]) {
+			// if clicked on wrong color:
+			// if strict, game over
+			if (this.state.isStrict) {
+				// if strict mode, end game // TODO - flash message to indicate user
+				this.endGame();
+				return;
+			}
+			// if not strict, restart sequence
+			this.restartSequence();
+			this.setState({
+				gameMessage: MESSAGE['wrong']
+			});
+			return;
+		}
+
+		// check if last color in sequence. if so, restart game
+		const newStepCount = stepCount + 1;
+		if (newStepCount === sequence.length) {
+			this.setState({
+				gameMessage: MESSAGE['correct']
+			})
+			setTimeout(() => {
+				// set highScore
+				const previousHighScore = this.state.highScore;
+				const currentScore = this.state.stepCount + 1;
+				// if at last sequence, 1 reset stepCount, 2 add a new color to sequence, and 3 call start() to restart game
+				this.setState(prevState => {
+					return {
+						stepCount: 0,
+						buttonClickable: false,
+						highScore: currentScore > previousHighScore ? currentScore : previousHighScore
+					};
+				});
+				this.startGame(true);
+			}, 2000);
+			return;
+		}
+		this.setState({
+			stepCount: newStepCount,
+		});
+		return;
+	};
+
+	render() {
+		const buttons = this.state.colors.map((buttonColor, index) => {
+			return (
+				<Button
+					style={{ backgroundColor: buttonColor }} // eslint-disable-next-line
+					className={'colorBtn ' + 'color' + index}
+					color={buttonColor}
+					key={index}
+					onMouseDown={() => this.handleColorMouseDown(buttonColor, index)}
+					clickable={this.state.buttonClickable}
+					index={index}
+				/>
+			);
+		});
+		return (
+			<div className="container">
+				<div className="sidebar">
+					<h1> Light and sound memory game 
+						
+					</h1>
+					<h3>
+						High Score: <span className="highScore">{this.state.highScore}</span>
+					</h3>
+					<h3>
+						Current Round: <span className="currentRound">{this.state.sequence.length}</span>
+					</h3>
+					
+					<div className="ctrlBtn">
+						<button
+							className="gameBtn"
+							onClick={this.startGame}
+							disabled={this.state.gameStarted}
+							style={this.state.gameStarted ? { opacity: '0.3' } : null}
+						>
+							Start
+						</button>
+						<button className="gameBtn" onClick={this.endGame} disabled={!this.state.gameStarted} style={this.state.gameStarted ? null : { opacity: '0.3' }}>
+							End
+						</button>
+					</div>
+					<div className="message">{this.state.gameMessage}</div>
+				</div>
+				<div className="game">
+					{buttons}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Game;
+
+// ================== helpers ==================
+// generates a random color inside COLORARR
+function generateNextColor(colors) {
+	return colors[Math.floor(Math.random(colors.length) * 4)];
+}
+
+// play audio
+function playSoundAtIndex(index) {
+	SOUNDS[index].play();
+}
